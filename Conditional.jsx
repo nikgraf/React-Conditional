9b46f315d834f40ea6b694a701bc29748d5fe150
Johannes Nagl <johannes@nagl.name>
2016-01-30 12:46:22 +0100

    basic performance optimization

--- Conditional.jsx
+++ Conditional.jsx
@@ -1,6 +1,14 @@
 import React, { PropTypes } from 'react';
 
 class Conditional extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    if (this.props.condition === false && nextProps.condition === false) {
+      return false;
+    }
+
+    return true;
+  }
+
   render() {
     return (this.props.condition) ? this.props.children : null;
   }
